df0f18cfb24026788defa9e2b2c185fa72ad4d1c
tarrasjose <tarras_souza@hotmail.com>
2023-01-12 11:47:13 -0300

    Create script.js

---
+++ 28-menu-mobile/modules-introducao/js/script.js
@@ -0,0 +1,22 @@
+import initeScrollSuave from "./modules/scroll-suave.js";
+import initeAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+
+
+
+
+initeScrollSuave();
+initeAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+
+
