84e7d2855a7aec771953b8969c3524802313101c
tarrasjose <tarras_souza@hotmail.com>
2023-01-12 11:45:26 -0300

    Create script.js

---
+++ 26-dropdown-menu/modules-introducao/js/script.js
@@ -0,0 +1,20 @@
+import initeScrollSuave from "./modules/scroll-suave.js";
+import initeAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+
+
+
+
+initeScrollSuave();
+initeAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+
+
