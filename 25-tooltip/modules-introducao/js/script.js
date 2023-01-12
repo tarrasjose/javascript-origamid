bca6496a24872417193e6670f614fc120d10c5a0
tarrasjose <tarras_souza@hotmail.com>
2023-01-12 11:43:47 -0300

    Create script.js

---
+++ 25-tooltip/modules-introducao/js/script.js
@@ -0,0 +1,18 @@
+import initeScrollSuave from "./modules/scroll-suave.js";
+import initeAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+
+
+
+initeScrollSuave();
+initeAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+
+
+
